b57e2487deaf27ee7304496c762fda9394c98731
david <davidally94@gmail.com>
2019-02-09 23:13:32 -0500

    Defined display function

--- assignment2.py
+++ assignment2.py
@@ -3,7 +3,7 @@
 
 import csv
 import urllib2
-import datetime
+from datetime import datetime
 import logging
 import argparse
 from pprint import pprint
@@ -21,25 +21,41 @@ def processData(data=downloadData(URL)):
 
     new_data = {}
     counter = 1
-
     csv_data = csv.DictReader(data)
+    error_logger = logging.getLogger('assignment2')
+
     for row in csv_data:
         try:
             for key, val in row.iteritems():
                 if key == 'birthday':
-                    birthday = datetime.datetime.strptime(val, '%m/%d/%Y')
+                    birthday = datetime.strptime(val, '%m/%d/%Y')
                 if key == 'id':
                     id_num = int(val)
                 if key == 'name':
                     name = val
             new_data[id_num] = (name, birthday)
         except Exception:
-            print 'ERROR: Date format issue with ID#: {}.'.format(counter)
+            error_logger.error('Error processing line #{} for ID #{}'.format(
+                csv_data.line_num, counter))
             counter += 1
             continue
         counter += 1
 
-    pprint(new_data)
+    return new_data
 
 
-processData()
+def displayPerson(id_num, personData):
+
+    try:
+        user_id = id_num
+        name = personData[0]
+        birthday = personData[1]
+        print 'Person #{} is {} with a birthday of {}.'.format(
+            user_id, name, str(birthday.date()))
+    except LookupError as err:
+        print "No user found with that ID."
+    except Exception as err:
+        print err
+
+
+def main()
